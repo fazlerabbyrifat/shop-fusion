7a1463b07f3b05cf0ba8d2580a8d23b2e8df3332
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-07-09 02:40:55 +0600

    all products is added

---
+++ src/pages/Products/Products.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const handleDetails = (id) => {
+    navigate(`/products/${id}`);
+  };
+
+  return (
+    <div className="overflow-x-auto my-10 mx-5 p-5">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Image</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, index) => (
+            <tr key={product.id}>
+              <td>{index + 1}</td>
+              <td>
+                <div className="flex items-center">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img src={product.image} alt="" />
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td>{product.name}</td>
+              <td>${product.price}</td>
+              <td>
+                <button
+                  onClick={() => handleDetails(product.id)}
+                  className="btn btn-primary"
+                >
+                  See More
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Products;
--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <Link to="/">Home</Link>
       </li>
       <li>
-        <Link to="/">Products</Link>
+        <Link to="/products">Products</Link>
       </li>
       <li>
         <Link to="/">
